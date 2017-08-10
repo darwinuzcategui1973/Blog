46da3bb0a78393d68e9be5a391352d934991ef51
Filiberto <fili06604@gmail.com>
2017-08-10 02:42:37 -0500

    🎨 Improving the code, much to go

--- src/js-dev/scripts.js
+++ src/js-dev/scripts.js
@@ -1,10 +1,15 @@
-$('#open-menu').click(function() {
-  $('#menu').addClass('menu-active');
+var $openMenu = document.getElementById("open-menu"),
+    $closeMenu = document.getElementById("close-menu"),
+    $menu = document.getElementById("menu");
+
+$openMenu.addEventListener("click", function(){
+    $menu.classList.add("menu-active");
 });
 
-$('#close-menu').click(function() {
-  $('#menu').removeClass('menu-active');
+$closeMenu.addEventListener("click", function(){
+    $menu.classList.remove("menu-active");
 });
+
 
 var div = document.createElement('div');
 document.body.appendChild(div);
